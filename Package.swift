6ca0f1beea2a7e1132f3c001f4e5b06ebb2e50cf
GrantBirki <grant.birkinbine@gmail.com>
2026-01-15 08:27:35 -0800

    Add SwiftLint configuration and format Package.swift for consistency

--- Package.swift
+++ Package.swift
@@ -3,25 +3,25 @@
 import PackageDescription
 
 let package = Package(
-  name: "OneShot",
-  platforms: [
-    .macOS(.v14)
-  ],
-  products: [
-    .executable(
-      name: "OneShot",
-      targets: ["OneShot"]
-    )
-  ],
-  targets: [
-    .executableTarget(
-      name: "OneShot",
-      path: "Sources"
-    ),
-    .testTarget(
-      name: "OneShotTests",
-      dependencies: ["OneShot"],
-      path: "Tests"
-    ),
-  ]
+    name: "OneShot",
+    platforms: [
+        .macOS(.v14),
+    ],
+    products: [
+        .executable(
+            name: "OneShot",
+            targets: ["OneShot"],
+        ),
+    ],
+    targets: [
+        .executableTarget(
+            name: "OneShot",
+            path: "Sources",
+        ),
+        .testTarget(
+            name: "OneShotTests",
+            dependencies: ["OneShot"],
+            path: "Tests",
+        ),
+    ],
 )
